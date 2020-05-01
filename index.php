1014936db8ebd308d4e572852b2fa0a3a7819604
SEB196 <53500154+SEB196@users.noreply.github.com>
2020-04-30 22:02:18 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+	<html>
+	<head>
+	  <title>Login</title>
+	  <link rel="stylesheet" type="text/css" href="estilos.css">
+	</head>
+	<body>
+	  <div id="login">
+	    <form action="usuario1.php" method="GET">
+	      
+	      <label>Usuario: </label>
+	      
+	      <input type="text" name="user" /><br>
+	      
+	      <label>Contraseña: </label>
+	      
+	      <input type="password" name="password" /><br><br>
+	      
+	      <input type="submit" value="Enviar" />
+	      
+	    </form>
+	    
+	  </div>
+	  
+	</body>
+	
+
+	</html>
